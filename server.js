e71af64449d04cc65d4313ba46c07ad774057f47
Glitch (smiling-mallet) <none>
2018-07-21 08:09:13 +0000

    ⛲️😽 Updated with Glitch

--- server.js
+++ server.js
@@ -151,6 +151,6 @@ function fractionThruYear(day,isLeapYear){
     numerator:Math.ceil(closest_match_value),
     denominator:closest_match,
     closeness:(closest_match_value%1).toPrecision(3),
-    approximate:closest_match==days_in_year
+    approximate:closest_match!=days_in_year
   }
 }
